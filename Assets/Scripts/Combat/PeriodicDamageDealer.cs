a583e6e0a97cc107e9025a02f45fb8fb753f53f1
Заболотский Семён <124987373+chumba1974@users.noreply.github.com>
2024-07-18 13:38:43 +0300

    Initial commit

---
+++ Assets/Scripts/Combat/PeriodicDamageDealer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+namespace BHSCamp
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class PeriodicDamageDealer : MonoBehaviour
+    {
+        [Header("MODE: Periodic damage")]
+        [SerializeField] private int _periodicDamage;
+        [SerializeField] private float _periodicDamageCooldown;
+        private float _periodicDamageTimer;
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            DealPeriodicDamage(collision.gameObject.GetComponent<IDamageable>());
+            _periodicDamageTimer += Time.fixedDeltaTime;
+        }
+
+        // STEP 9: Сейчас урон наносится каждый раз, когда вызывается OnTriggerStay2D,
+        // т.е. каждый кадр обработки физики
+        // сделайте так, чтобы урон наносился каждые _periodicDamageCooldown секунд (по таймеру)
+
+        private void DealPeriodicDamage(IDamageable damageable)
+        {
+            if (damageable == null) return;
+
+            if (_periodicDamageTimer >= _periodicDamageCooldown)
+            {
+                _periodicDamageTimer = 0;
+                damageable.TakeDamage(_periodicDamage);
+                MonoBehaviour mb = (MonoBehaviour)damageable;
+                print($"Dealt {_periodicDamage} to {mb.name}");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/States/IdleState.cs
@@ -0,0 +1,35 @@
+using BHSCamp.FSM;
+
+namespace BHSCamp
+{
+    public class IdleState : FsmState
+    {
+        private PatrolEnemy _enemy;
+        private float _exitTime;
+        private float _timer;
+
+        public IdleState(Fsm fsm, PatrolEnemy enemy, float exitTime) : base(fsm)
+        {
+            _enemy = enemy;
+            _exitTime = exitTime;
+        }
+
+        public override void Enter()
+        {
+            _timer = 0;
+        }
+
+        // STEP 4: Сейчас из данного состояния можно перейти только в PatrolState
+        // сделайте так, чтобы, при обнаружении игрока, осуществлялся переход в AttackState
+        public override void Update(float deltaTime)
+        {
+            _timer += deltaTime;
+            if (_timer > _exitTime)
+                Fsm.SetState<PatrolState>();
+
+            if (_enemy is EnemyWithAttack e && e.PlayerInSight())
+                Fsm.SetState<AttackState>();
+        }
+
+    }
+}
